12f8fa4bcc99e5137e11168d364c2d6f5ca5cab1
tylersalvato <tyler.harrissalvato@sdgku.edu>
2023-10-13 01:27:09 -0400

    second

---
+++ app/routes.py
@@ -0,0 +1,53 @@
+from flask import Flask, request
+from app.database import task
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return {
+        "ok": True,
+        "timestamp": datetime.now().strftime("%F %H:%M:%S")
+    }
+
+@app.get("/tasks")
+def get_all_tasks():
+    tasks = task.scan()
+    out = {
+        "tasks": tasks,
+        "ok": True
+    }
+    return out
+
+@app.get("/tasks/<int:pk>")
+def get_task_by_id(pk):
+    single_task = task.select_by_id(pk)
+    if not single_task:
+        return {"ok": False, "message": "Not found"}, 404
+    out = {
+        "task": single_task,
+        "ok": True
+    }
+    return out
+
+@app.post("/tasks")
+def create_task():
+    task_data = request.json
+    task.insert(task_data)
+    out = {
+        "ok": True,
+        "message": "Success"
+    }
+    return out, 201
+
+@app.put("/tasks/<int:pk>")
+def update_task(pk):
+    task_data = request.jsontask.update_by_id(pk, task_data)
+    return "", 204
+
+@app.delete("/tasks/<int:pk>")
+def delete_task(pk):
+    task.delete(pk)
+    return "", 204
---
+++ utils/create_task.py
@@ -0,0 +1,23 @@
+import requests
+
+BACKEND_URL = "http://127.0.0.1:5000/tasks"
+
+
+def create_task(summary, description):
+    task = {
+        "summary": summary,
+        "description": description
+    }
+    response = requests.post(BACKEND_URL, json=task)
+    if response.status_code == 201:
+        print("Creation succeeded.")
+    else:
+        print("Creation failed.")
+
+
+if __name__ == "__main__":
+    print("Fill out the prompt below to create a new task:")
+    summary = input("New task summary: ")
+    description = input("New task description: ")
+    create_task(summary, description)
+    
